af3fea5b7cd9942a6a929c4b9ab27c09fe719eec
Viktor <stanko181@gmail.com>
2019-10-14 15:24:50 +0700

    Add header with block register and enter

---
+++ index.js
@@ -0,0 +1,12 @@
+//pug
+import './index.pug'
+//scss
+import './scss/style.scss'
+//jQuery
+import 'jquery'
+//js plugin
+import 'magnific-popup'
+import 'air-datepicker'
+//js
+import './header/js/header'
+import './header/buttons/btn_register/js/datepicker'
