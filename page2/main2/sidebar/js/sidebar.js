75f57f27067036d58a4e0807aef5c6740b0673c6
Viktor <stanko181@gmail.com>
2019-10-24 17:44:38 +0700

    Add pagination for page with numbers.

---
+++ page2/main2/sidebar/js/sidebar.js
@@ -0,0 +1,5 @@
+$('.facilities').countFacilities();
+
+$('.click__additional-amenities').on('click', function () {
+    $('.additional-amenities').css('display') == 'none' ? $('.additional-amenities').animate({height: 'show'}, 400) : $('.additional-amenities').animate({height: 'hide'}, 200);
+});
