cd31f8060642c06a2d0c42cd0139ef4c372a8014
Viktor <stanko181@gmail.com>
2019-10-29 17:08:25 +0700

    Page 3
    Add review block and reservation block
    Add rules and cancel block

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ import 'jquery'
 //js plugin
 import 'magnific-popup'
 import 'air-datepicker'
-import './main/number-for-you/js/count/count'
 import './page2/main2/sidebar/js/count_facilities/count_facilities'
 import './page2/main2/js/raty-2.4.5/js/jquery.raty'
+import './main/number-for-you/js/count/count'
 //js
 
 import './header/js/header'
@@ -20,4 +20,5 @@ import './main/number-for-you/js/number-for-you'
 import './page2/main2/sidebar/js/sidebar'
 import './page3/main3/js/main3'
 import './page2/main2/js/pagination'
+import './page3/main3/room-reservation/js/room-reservation'
 
