14f58fc572e223b42b723caa1588e5d43ce8c197
Viktor <stanko181@gmail.com>
2019-10-30 15:15:53 +0700

    Comleted

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ import 'air-datepicker'
 import './page2/main2/sidebar/js/count_facilities/count_facilities'
 import './page2/main2/js/raty-2.4.5/js/jquery.raty'
 import './main/number-for-you/js/count/count'
+import 'chart.js'
 //js
 
 import './header/js/header'
 import './header/buttons/btn_register/js/datepicker'
 import './main/number-for-you/js/number-for-you'
 import './page2/main2/sidebar/js/sidebar'
-import './page3/main3/js/main3'
+
 import './page2/main2/js/pagination'
+import './page3/main3/js/main3'
 import './page3/main3/room-reservation/js/room-reservation'
 
