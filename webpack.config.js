9498ea9353938013395edc3e05427f90c948ee18
Viktor <stanko181@gmail.com>
2019-10-31 14:03:46 +0700

    Fix path

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
+const sass = require('sass-loader')
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin({}),
+            new UglifyJsPlugin()
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 4200
+    },
+    plugins: [
+        new HTMLPlugin({
+            filename: 'index.html',
+            template: './src/index.pug'
+        }),
+        new HTMLPlugin({
+            filename: 'page2.html',
+            template: './src/page2.pug'
+        }),
+        new HTMLPlugin({
+            filename: 'page3.html',
+            template: './src/page3.pug'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: "babel-loader" 
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'img/[name].[ext]'
+                }
+            }
+        ]
+    }
+}
